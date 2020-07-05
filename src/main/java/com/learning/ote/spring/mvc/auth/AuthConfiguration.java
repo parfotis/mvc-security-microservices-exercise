4bd3f99c83956ecd7c95a9fc87d409cfbcce3c03
Giannis Kavvouras <giannis.kavvouras@camelotglobal.com>
2020-07-05 17:23:58 +0300

    [10_4_annotaion_authorization]: uses PreAuthorize annotation to check admin user role

--- src/main/java/com/learning/ote/spring/mvc/auth/AuthConfiguration.java
+++ src/main/java/com/learning/ote/spring/mvc/auth/AuthConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +14,7 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AuthConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -21,17 +22,6 @@ public class AuthConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().and()
-                .httpBasic();
-    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
--- src/main/java/com/learning/ote/spring/mvc/controller/UsersController.java
+++ src/main/java/com/learning/ote/spring/mvc/controller/UsersController.java
@@ -1,6 +1,7 @@
 package com.learning.ote.spring.mvc.controller;
 
 import com.learning.ote.spring.mvc.service.UsersService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ public class UsersController {
     }
 
     @GetMapping("/")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String getAllUsers(Model model) {
         model.addAttribute("users", usersService.getUsers());
         return "users";

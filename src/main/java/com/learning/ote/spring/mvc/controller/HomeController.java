eb44be1babf82be100c2f7caf761c193e129b777
Giannis Kavvouras <giannis.kavvouras@camelotglobal.com>
2020-07-05 19:08:46 +0300

    [10_3_authorization]: fixes merge conflicts

--- src/main/java/com/learning/ote/spring/mvc/controller/HomeController.java
+++ src/main/java/com/learning/ote/spring/mvc/controller/HomeController.java
@@ -2,16 +2,30 @@ package com.learning.ote.spring.mvc.controller;
 
 import com.learning.ote.spring.mvc.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
 
     @Autowired
     private UsersService usersService;
+
+    @ModelAttribute
+    public void securityContext(Model model) {
+        List<String> authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+        model.addAttribute("_authorities", authorities);
+    }
 
     @GetMapping({"/"})
     public String home(Model model) {

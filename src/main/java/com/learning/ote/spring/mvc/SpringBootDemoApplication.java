dca527d0ab6f78fcac6077f5668a91fefc326cfe
Fotis Paraskevas <parfotis@hotmail.com>
2020-09-11 20:12:10 +0300

    added dependencies for JspTaglibs (Spring Security in FreeMarker) and allowed links for ROLE_ADMIN only)

--- src/main/java/com/learning/ote/spring/mvc/SpringBootDemoApplication.java
+++ src/main/java/com/learning/ote/spring/mvc/SpringBootDemoApplication.java
@@ -2,9 +2,16 @@ package com.learning.ote.spring.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import static java.util.Collections.singletonList;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
+
+	public SpringBootDemoApplication(FreeMarkerConfigurer freeMarkerConfigurer) {
+		freeMarkerConfigurer.getTaglibFactory().setClasspathTlds(singletonList("/META-INF/security.tld"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
